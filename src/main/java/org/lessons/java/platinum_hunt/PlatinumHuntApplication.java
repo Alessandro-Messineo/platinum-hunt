4ca7fc30572cd475b622234e7c8a914fda7b950b
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-03-02 17:36:26 +0100

    first commit

---
+++ src/main/java/org/lessons/java/platinum_hunt/PlatinumHuntApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.platinum_hunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatinumHuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatinumHuntApplication.class, args);
+	}
+
+}
