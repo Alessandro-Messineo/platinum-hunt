14d09f30dfb595d6f073f2181ff6d71e97165176
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-03-03 15:45:52 +0100

    fix navbar e game controller

--- src/main/java/org/lessons/java/platinum_hunt/controller/GameController.java
+++ src/main/java/org/lessons/java/platinum_hunt/controller/GameController.java
@@ -1,6 +1,6 @@
 package org.lessons.java.platinum_hunt.controller;
 
-import org.lessons.java.platinum_hunt.model.Game;
+
 import org.lessons.java.platinum_hunt.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
