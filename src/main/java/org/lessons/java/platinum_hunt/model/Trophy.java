73691869a009e127d2279ca85e5934a456fb1229
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-03-04 18:11:08 +0100

    fix

--- src/main/java/org/lessons/java/platinum_hunt/model/Trophy.java
+++ src/main/java/org/lessons/java/platinum_hunt/model/Trophy.java
@@ -1,7 +1,6 @@
 package org.lessons.java.platinum_hunt.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
