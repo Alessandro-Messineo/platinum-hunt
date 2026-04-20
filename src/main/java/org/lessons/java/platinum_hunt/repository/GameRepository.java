e988a078cdaf485b4d493c66ca316a08756a0dfa
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-04-20 17:26:53 +0200

    aggiunto metodo personalizzato a gameRepository

--- src/main/java/org/lessons/java/platinum_hunt/repository/GameRepository.java
+++ src/main/java/org/lessons/java/platinum_hunt/repository/GameRepository.java
@@ -2,7 +2,10 @@ package org.lessons.java.platinum_hunt.repository;
 
 import org.lessons.java.platinum_hunt.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface GameRepository extends JpaRepository<Game, Integer>{
     
+    public List<Game> findByName(String name);
 }
